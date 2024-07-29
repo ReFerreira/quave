e9e6b9963ae1e027f4bb9358d346c37870083f0c
Reinaldo Ferreira <rgferreira90@gmail.com>
2024-07-29 23:33:26 +0000

    fix: tailwind css

--- ui/components/Summary/Summary.js
+++ ui/components/Summary/Summary.js
@@ -6,16 +6,21 @@ export const Summary = ({ eventPeople }) => {
     return null;
   }
 
-  const peopleInEvent = eventPeople.filter(person => person.checkInDate && !person.checkOutDate).length;
-  
-  const peopleNotCheckedIn = eventPeople.filter(person => !person.checkInDate).length;
+  const peopleInEvent = eventPeople.filter(
+    (person) => person.checkInDate && !person.checkOutDate
+  ).length;
+
+  const peopleNotCheckedIn = eventPeople.filter(
+    (person) => !person.checkInDate
+  ).length;
+
   const peopleByCompany = eventPeople.reduce((acc, person) => {
     if (person.checkInDate && !person.checkOutDate && person.companyName) {
       acc[person.companyName] = (acc[person.companyName] || 0) + 1;
     }
     return acc;
   }, {});
-  
+
   // Crie a string fora do JSX
   const peopleByCompanyString = Object.entries(peopleByCompany)
     .map(([companyName, count]) => `${companyName} (${count})`)
@@ -24,9 +29,11 @@ export const Summary = ({ eventPeople }) => {
   return (
     <div className="summary">
       <h3>People in the event right now: {peopleInEvent}</h3>
-      <h3>People by company in the event right now: {peopleByCompanyString || "None"}</h3>
+      <h3>
+        People by company in the event right now:{' '}
+        {peopleByCompanyString || 'None'}
+      </h3>
       <h3>People not checked in: {peopleNotCheckedIn}</h3>
     </div>
   );
 };
-
