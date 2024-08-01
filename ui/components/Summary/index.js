30c71352067778faf6ba9dd1b248b86bd7c94c87
Reinaldo Ferreira <rgferreira90@gmail.com>
2024-08-01 23:58:55 +0000

    refactor: Summary

--- ui/components/Summary/index.js
+++ ui/components/Summary/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+
 
 export const Summary = ({ eventPeople }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   if (!eventPeople || eventPeople.length === 0) {
     return null;
   }
@@ -21,17 +25,38 @@ export const Summary = ({ eventPeople }) => {
   }, {});
 
   // Crie a string fora do JSX
-  const peopleByCompanyString = Object.entries(peopleByCompany)
-    .map(([companyName, count]) => `${companyName} (${count})`)
-    .join(', ');
+  const peopleByCompanyString = Object.entries(peopleByCompany);
 
   return (
-    <div className="text-gray-800 mb-6">
+    <div className="mb-6 text-gray-800">
       <h3>People in the event right now: {peopleInEvent}</h3>
-      <h3>
-        People by company in the event right now:{' '}
-        {peopleByCompanyString || 'None'}
-      </h3>
+
+      <h3>People by company in the event right now:</h3>
+      {peopleByCompanyString.map(([company, quantity], index) => {
+        if (!isExpanded && index > 3) return null
+
+        return (
+          <li key={Math.random()}>
+            {company}: {quantity}
+          </li>
+        )
+
+      })}
+
+      {peopleByCompanyString.length > 3 && (
+        <div onClick={() => setIsExpanded(!isExpanded)} className='cursor-pointer'>
+          {!isExpanded ? (
+            <div className="flex items-center">
+              <span className='mr-1.25'>ver mais</span> <BsChevronDown />
+            </div>
+          ) : (
+            <div className="flex items-center">
+              <span className='mr-1.25'>ver menos</span> <BsChevronUp />
+            </div>
+          )}
+        </div>
+      )}
+
       <h3>People not checked in: {peopleNotCheckedIn}</h3>
     </div>
   );
