df9ad4f34db6b3ad1b1c1835f2757bb1d11b55d3
Reinaldo Ferreira <reinaldo.ferreira@ifood.com.br>
2024-08-02 22:25:33 -0300

    refactor: layout

--- ui/componentes/PeopleList/index.js
+++ ui/componentes/PeopleList/index.js
@@ -1,57 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const PeopleList = ({
   eventPeople,
   handleCheckIn,
   handleCheckOut,
   checkInTimes,
-}) => (
-  <ul className="people-list">
-    {eventPeople.map((person) => {
-      const checkInDate = new Date(person.checkInDate);
-      const now = new Date();
-      const shouldShowCheckOutButton =
-        person.checkInDate && !person.checkOutDate && now - checkInDate > 5000;
-      return (
-        <li
-          key={person._id}
-          className="mb-2 rounded border border-gray-300 p-2"
-        >
-          <div>
-            {person.firstName} {person.lastName}
-          </div>
-          <div>{person.company}</div>
-          <div>{person.title}</div>
-          <div>
-            Check-in date:{' '}
-            {person.checkInDate
-              ? new Date(person.checkInDate).toLocaleString()
-              : 'N/A'}
-          </div>
-          <div>
-            Check-out date:{' '}
-            {person.checkOutDate
-              ? new Date(person.checkOutDate).toLocaleString()
-              : 'N/A '}
-          </div>
-          {!person.checkInDate && (
-            <button
-              onClick={() => handleCheckIn(person._id)}
-              className="my-1.5 rounded bg-green-500 px-2 py-1 text-white"
+}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  if (!eventPeople || eventPeople.length === 0) {
+    return null;
+  }
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredEventPeople = eventPeople
+    .filter((person) => {
+      const fullName = `${person.firstName} ${person.lastName}`.toLowerCase();
+      return fullName.includes(searchTerm.toLowerCase());
+    })
+    .sort((a, b) => {
+      const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
+      const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+
+  return (
+    <div className="flex flex-col justify-end">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Search by name"
+        className="mb-4 ml-auto rounded border border-gray-300 p-2"
+      />
+      <ul className="people-list">
+        {filteredEventPeople.map((person) => {
+          const checkInDate = new Date(person.checkInDate);
+          const now = new Date();
+          const shouldShowCheckOutButton =
+            person.checkInDate &&
+            !person.checkOutDate &&
+            now - checkInDate > 5000;
+          return (
+            <li
+              key={person._id}
+              className="mb-2 rounded border border-gray-300 p-2"
             >
-              Check-in {person.firstName} {person.lastName}
-            </button>
-          )}
-          {checkInTimes && shouldShowCheckOutButton && (
-            <button
-              onClick={() => handleCheckOut(person._id)}
-              className="my-1.5 rounded bg-red-500 px-2 py-1 text-white"
-            >
-              Check-out {person.firstName} {person.lastName}
-            </button>
-          )}
-        </li>
-      );
-    })}
-  </ul>
-);
+              <div>
+                {person.firstName} {person.lastName}
+              </div>
+              <div>{person.company}</div>
+              <div>{person.title}</div>
+              <div>
+                Check-in date:{' '}
+                {person.checkInDate
+                  ? new Date(person.checkInDate).toLocaleString()
+                  : 'N/A'}
+              </div>
+              <div>
+                Check-out date:{' '}
+                {person.checkOutDate
+                  ? new Date(person.checkOutDate).toLocaleString()
+                  : 'N/A'}
+              </div>
+              {!person.checkInDate && (
+                <button
+                  onClick={() => handleCheckIn(person._id)}
+                  className="my-1.5 rounded bg-green-500 px-2 py-1 text-white"
+                >
+                  Check-in {person.firstName} {person.lastName}
+                </button>
+              )}
+              {checkInTimes && shouldShowCheckOutButton && (
+                <button
+                  onClick={() => handleCheckOut(person._id)}
+                  className="my-1.5 rounded bg-red-500 px-2 py-1 text-white"
+                >
+                  Check-out {person.firstName} {person.lastName}
+                </button>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
